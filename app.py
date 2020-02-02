3b583f7fc819c576340bd69968064142c06567c1
Weslei Silva Bomfim <www.weslei@gmail.com>
2020-02-02 11:52:35 -0300

    Start of creation user interface

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from model.db import Db
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Faça. Ou não faça. Não existe a tentativa.'
 app.config["DEBUG"] = True
@@ -8,7 +9,14 @@ app.config["DEBUG"] = True
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    d = [356.56, 569.58, 365.56]
+    position = Db()
+    return render_template("index.html", position = position.position(), d = d )
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
---
+++ tests/testdb.py
@@ -0,0 +1,5 @@
+from model import db
+
+p =db.Db()
+
+print(p.position())
